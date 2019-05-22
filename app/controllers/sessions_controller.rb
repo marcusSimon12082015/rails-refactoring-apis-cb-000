9b57d96ccf463a6758727dd3fe72c69ab010a6cc
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-05-22 13:18:03 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,15 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user, only: :create
 
   def create
-    response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["GITHUB_CLIENT"], client_secret: ENV["GITHUB_SECRET"],code: params[:code]}, {'Accept' => 'application/json'}
-    access_hash = JSON.parse(response.body)
-    session[:token] = access_hash["access_token"]
+    github_service = GithubService.new
+    session[:token] = github_service.authenticate!(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_SECRET'], params[:code])
+    #response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["GITHUB_CLIENT"], client_secret: ENV["GITHUB_SECRET"],code: params[:code]}, {'Accept' => 'application/json'}
+    #access_hash = JSON.parse(response.body)
+    #session[:token] = access_hash["access_token"]
 
     #user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
     #user_json = JSON.parse(user_response.body)
     #session[:username] = user_json["login"]
-    github_service = GithubService.new
     session[:username] = github_service.get_username
     redirect_to '/'
   end
