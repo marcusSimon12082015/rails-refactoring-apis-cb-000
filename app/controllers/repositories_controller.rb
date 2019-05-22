b02cd7ddde5a629eb792f82d6f752ad9561bd1f0
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-05-22 13:22:41 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,9 @@
 class RepositoriesController < ApplicationController
   def index
-    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-    @repos_array = JSON.parse(response.body)
+    github_service = GithubService.new
+    @repos_array = github_service.get_repos
+    #response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    #@repos_array = JSON.parse(response.body)
   end
 
   def create
